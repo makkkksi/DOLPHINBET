b242ef3c5a9b2189bde02a533cde36c5586c8ce8
makkkksi <maximiliano.santibanez@mail.udp.cl>
2025-09-01 15:47:20 -0400

    modal

---
+++ verificacion.js
@@ -0,0 +1,35 @@
+// Simulación de usuarios registrados
+const usuarios = [
+    { email: "usuario1@example.com", password: "123456" },
+    { email: "usuario2@example.com", password: "contraseña" }
+  ];
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginForm");
+  
+    if (form) {
+      form.addEventListener("submit", function (e) {
+        e.preventDefault();
+  
+        const email = document.getElementById("loginEmail").value.trim();
+        const password = document.getElementById("loginPassword").value;
+  
+        const usuarioValido = usuarios.find(
+          (u) => u.email === email && u.password === password
+        );
+  
+        const errorDiv = document.getElementById("loginError");
+  
+        if (usuarioValido) {
+          errorDiv.style.display = "none";
+          alert("¡Inicio de sesión exitoso!");
+          const modalElement = document.getElementById("loginModal");
+          const modal = bootstrap.Modal.getInstance(modalElement);
+          modal.hide();
+        } else {
+          errorDiv.textContent = "Correo o contraseña incorrectos.";
+          errorDiv.style.display = "block";
+        }
+      });
+    }
+  });
