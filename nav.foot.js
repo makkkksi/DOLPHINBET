45c9d7dd178b2c7f30d67ec00ca3bf63e1983678
makkkksi <maximiliano.santibanez@mail.udp.cl>
2025-10-17 17:35:27 -0300

    testing

--- nav.foot.js
+++ nav.foot.js
@@ -1,14 +1,23 @@
-// navbar-footer.js IMPORTA ESTO EL NAV Y EL FOOTER EN HTML COMO UN IBLOCK COMO LO VIMOS EN CLASES!!!1
+// navbar-footer.js
 function includeHTML(id, file) {
-    fetch(file)
-      .then(res => res.text())
-      .then(html => {
-        document.getElementById(id).innerHTML = html;
-      })
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    includeHTML("navbar", "nav.html");
-    includeHTML("footer", "footer.html");
+  return fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await includeHTML("navbar", "nav.html");
+  await includeHTML("footer", "footer.html");
+
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
   });
-  
+});
